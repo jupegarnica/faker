4fcdc46c15635663c481f8c01c1c94be6a32978c
Jupegarnica <juan@geekshubs.com>
2022-03-09 13:37:58 +0100

    feat: update logger

--- services/logger.ts
+++ services/logger.ts
@@ -1,7 +1,7 @@
-import logger, { pretty } from "https://deno.land/x/garn_logger/mod.ts";
+import logger, { formatToAnsiColors } from "https://deno.land/x/garn_logger@0.0.11/mod.ts";
 
 logger.setFilter(Deno.env.get("LOG_LEVEL") || "DEBUG");
-logger.use(pretty({ multiline: false }));
+logger.use(formatToAnsiColors({ multiline: false }));
 
 export { logger };
 export default logger;
