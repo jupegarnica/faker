11e1ff9bb449652913c1ba30aecc65c2bacabca3
Jupegarnica <juan@geekshubs.com>
2022-03-09 13:37:58 +0100

    format: apply auto format

--- services/logger.ts
+++ services/logger.ts
@@ -1,4 +1,6 @@
-import logger, { formatToAnsiColors } from "https://deno.land/x/garn_logger@0.0.11/mod.ts";
+import logger, {
+  formatToAnsiColors,
+} from "https://deno.land/x/garn_logger@0.0.11/mod.ts";
 
 logger.setFilter(Deno.env.get("LOG_LEVEL") || "DEBUG");
 logger.use(formatToAnsiColors({ multiline: false }));
