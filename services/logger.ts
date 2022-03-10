d771b03c062527beff7025330917349ba6d653f4
Jupegarnica <juan@geekshubs.com>
2022-03-10 16:02:38 +0100

    format: apply auto format

--- services/logger.ts
+++ services/logger.ts
@@ -3,16 +3,22 @@ import logger, {
 } from "https://deno.land/x/garn_logger@0.0.11/mod.ts";
 
 import { transportToEmail } from "https://deno.land/x/garn_logger@0.0.13/src/middleware/transport_to_email.ts";
-logger.setFilter(Deno.env.get("LOG_LEVEL") || "DEBUG");
+logger.setFilter(
+  Deno.env.get("LOG_LEVEL") || "DEBUG",
+);
 logger.use(
-  formatToAnsiColors({ multiline: false }),
+  formatToAnsiColors({
+    multiline: false,
+  }),
   transportToEmail({
-    hostname: Deno.env.get("SMTP_HOST") || "",
-    port: Deno.env.get("SMTP_PORT") || "",
+    hostname:
+      Deno.env.get("SMTP_HOST") || "",
+    port: Deno.env.get("SMTP_PORT") ||
+      "",
     user: Deno.env.get("SMTP_USER"),
     password: Deno.env.get("SMTP_PASS"),
-  })
-  );
+  }),
+);
 
 export { logger };
 export default logger;
