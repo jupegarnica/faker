55a1c6af252999057f433a48d2be9d612530c235
Jupegarnica <juan@geekshubs.com>
2022-03-10 17:26:07 +0100

    feat: email everything

--- services/logger.ts
+++ services/logger.ts
@@ -11,13 +11,14 @@ logger.use(
     multiline: false,
   }),
   transportToEmail({
-    hostname: Deno.env.get("SMTP_HOST") || "",
-    port: Deno.env.get("SMTP_PORT") ||
-      "",
-    username: Deno.env.get("SMTP_USER") || "",
+    hostname: Deno.env.get("SMTP_HOST") || "localhost",
+    port: Deno.env.get("SMTP_PORT") || "1025",
+    username: Deno.env.get("SMTP_USER"),
     password: Deno.env.get("SMTP_PASS"),
     to: "juan@garn.dev",
     from: "juan@garn.dev",
+    logLevel: "DEBUG",
+    debounceTime: 3000,
   }),
 );
 
