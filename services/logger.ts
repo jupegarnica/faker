3ab1cb0da0633762d55a83eef59324342da537a6
Jupegarnica <juan@geekshubs.com>
2022-03-11 16:42:42 +0100

    feat: clean up

--- services/logger.ts
+++ services/logger.ts
@@ -6,20 +6,21 @@ import { transportToEmail } from "https://deno.land/x/garn_logger@0.0.13/src/mid
 logger.setFilter(
   Deno.env.get("LOG_LEVEL") || "DEBUG",
 );
+
 logger.use(
   formatToAnsiColors({
     multiline: false,
   }),
-  // transportToEmail({
-  //   hostname: Deno.env.get("SMTP_HOST") || "localhost",
-  //   port: Deno.env.get("SMTP_PORT") || "1025",
-  //   username: Deno.env.get("SMTP_USER"),
-  //   password: Deno.env.get("SMTP_PASS"),
-  //   to: "juan@garn.dev",
-  //   from: "juan@garn.dev",
-  //   logLevel: "DEBUG",
-  //   debounceTime: 3000,
-  // }),
+  transportToEmail({
+    hostname: Deno.env.get("SMTP_HOST") || "localhost",
+    port: Deno.env.get("SMTP_PORT") || "1025",
+    username: Deno.env.get("SMTP_USER"),
+    password: Deno.env.get("SMTP_PASS"),
+    to: Deno.env.get("SMTP_TO") || '',
+    from: Deno.env.get("SMTP_FROM") || '',
+    logLevel: "DEBUG",
+    debounceTime: 3000,
+  }),
 );
 
 export { logger };
