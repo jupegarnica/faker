433fd7b96dc741b8088f5ff2b9bf48ceb6a39115
Jupegarnica <juan@geekshubs.com>
2022-05-18 13:05:04 +0200

    chore: update deps

--- services/logger.ts
+++ services/logger.ts
@@ -2,7 +2,7 @@ import logger, {
   formatToAnsiColors,
 } from "https://deno.land/x/garn_logger@0.0.21/mod.ts";
 
-import { transportToEmail } from "https://deno.land/x/garn_logger@0.0.16/src/middleware/transport_to_email.ts";
+import { transportToEmail } from "https://deno.land/x/garn_logger@0.0.21/src/middleware/transport_to_email.ts";
 
 logger.setFilter(
   Deno.env.get("LOG_LEVEL") || "DEBUG",
