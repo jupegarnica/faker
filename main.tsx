cbf8ff3e7db5f1f2b1a641f3a555ba298687f9d2
Juan Garnica <juan@harbiz.io>
2024-11-02 10:21:16 +0100

    feat(main): refactor Help component and enhance error handling with detailed response structure; remove old Help implementation and update createFaker function

--- main.tsx
+++ main.tsx
@@ -7,29 +7,6 @@ import type { LocaleDefinition } from "npm:@faker-js/faker";
 const Faker = FakerNameSpace.Faker;
 
 const app = new Hono();
-type HelpProps = {
-  categories: { category: string; methods: string[] }[];
-};
-
-export function Help({ categories }: HelpProps) {
-  return (
-    <html>
-      <body>
-        <h1>Available endpoints:</h1>
-        {categories.map(({ category, methods }) => (
-          <div key={category}>
-            <h2>{category.toUpperCase()}:</h2>
-            {methods.map((method) => (
-              <p key={method}>
-                <a href={`/${category}/${method}/`}>/{category}/{method}/</a>
-              </p>
-            ))}
-          </div>
-        ))}
-      </body>
-    </html>
-  );
-}
 app.use(logger());
 // Middleware to handle status, body, headers, and delay
 app.use(async (context: Context, next) => {
@@ -79,7 +56,7 @@ app.use(async (context: Context, next) => {
 
 app.get("/", (context: Context) => {
   const language = context.req.header("Accept-Language") || "en";
-  const faker = createFaker(language);
+  const { faker } = createFaker(language);
   const categories: { category: string; methods: string[] }[] = [];
 
   // extract category and method from the faker instance
@@ -104,8 +81,8 @@ app.get("/", (context: Context) => {
 });
 
 app.all("/:category/:method/*", async (context: Context) => {
-  const language = context.req.header("Accept-Language") || "es";
-  const faker = createFaker(language);
+  const langRaw = context.req.header("Accept-Language") || "en";
+  const { faker, language } = createFaker(langRaw);
 
   const { category, method } = context.req.param();
   const args =
@@ -121,6 +98,7 @@ app.all("/:category/:method/*", async (context: Context) => {
       }) || [];
 
   try {
+    // @ts-ignore
     const fn = faker[category][method];
     if (!fn || typeof fn !== "function") {
       throw new Error(`Method ${category}.${method} not found. ${typeof fn}`);
@@ -129,23 +107,63 @@ app.all("/:category/:method/*", async (context: Context) => {
     console.log({ category, method, args, data });
     return context.json(data);
   } catch (error: unknown) {
-    console.error({ category, method, args, error });
-    const errorMessage = `Method ${category}.${method} failed:
-    ${error}
-
-    Faker docs: https://fakerjs.dev/api/${category}.html#${method}
-    `;
-    return context.text(errorMessage, 400);
+    console.error({ category, method, args, error, language });
+    return context.html(<ErrorHelp error={error as Error} category={category} method={method} args={args} />, 400);
   }
 });
 
-function createFaker(language: string): any {
-  const languageParsed = language.split(",")[0].split("-")[0];
-  const lang: LocaleDefinition =
-    // @ts-ignore
-    FakerNameSpace[languageParsed] || FakerNameSpace.es;
+Deno.serve(app.fetch);
 
-  return new Faker({ locale: [lang] }) as unknown as typeof Faker;
+type HelpProps = {
+  categories: { category: string; methods: string[] }[];
+};
+
+function Help({ categories }: HelpProps) {
+  return (
+    <html>
+      <body>
+        <h1>Available endpoints:</h1>
+        {categories.map(({ category, methods }) => (
+          <div key={category}>
+            <h2>{category.toUpperCase()}:</h2>
+            {methods.map((method) => (
+              <p key={method}>
+                <a href={`/${category}/${method}/`}>/{category}/{method}/</a>
+              </p>
+            ))}
+          </div>
+        ))}
+      </body>
+    </html>
+  );
 }
 
-Deno.serve(app.fetch);
+function ErrorHelp({ error, category, method, args }: { error: Error, category: string, method: string, args: any[] }) {
+  // const errorMessage = `Method ${category}.${method} failed:
+  //   ${error}
+
+  //   Faker docs: https://fakerjs.dev/api/${category}.html#${method}
+  //   `;
+  return (
+    <html>
+      <body>
+        <h1>Method {category}.{method} failed:</h1>
+        <p>{error}</p>
+        <p>Faker docs: <a href={`https://fakerjs.dev/api/${category}.html#${method}`}>https://fakerjs.dev/api/{category}.html#{method}</a></p>
+        <h2>Arguments:</h2>
+        <pre>{JSON.stringify(args, null, 2)}</pre>
+      </body>
+    </html>
+
+  );
+}
+
+function createFaker(languageRaw: string): { faker: typeof Faker, language: string } {
+  const language = languageRaw.split(",")[0].split("-")[0];
+  const lang: LocaleDefinition =
+    // @ts-ignore
+    FakerNameSpace[language] || FakerNameSpace.en;
+
+  const faker = new Faker({ locale: [lang, FakerNameSpace.en] }) as unknown as typeof Faker;
+  return { faker, language };
+}
