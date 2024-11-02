879bf3e3de3ccf3cd3408c2565281918561abb20
Juan Garnica <juan@harbiz.io>
2024-11-02 09:41:55 +0100

    feat(main): enhance main.tsx with logger middleware and dynamic response handling for status, body, and headers; add test.http for API validation

--- main.tsx
+++ main.tsx
@@ -1,4 +1,6 @@
 import { Hono, type Context } from "hono";
+import { logger } from 'hono/logger'
+import { StatusCode } from "hono/utils/http-status";
 import * as FakerNameSpace from "npm:@faker-js/faker";
 import type { LocaleDefinition } from "npm:@faker-js/faker";
 
@@ -28,6 +30,52 @@ export function Help({ categories }: HelpProps) {
     </html>
   );
 }
+app.use(logger());
+// Middleware to handle status, body, headers, and delay
+app.use(async (context: Context, next) => {
+  const url = new URL(context.req.url);
+  const status = url.searchParams.get("status");
+  const body = url.searchParams.get("body");
+  const headers = url.searchParams.get("headers");
+  const delay = url.searchParams.get("delay");
+
+  if (headers) {
+    try {
+      const parsedHeaders = JSON.parse(headers);
+      for (const [key, value] of Object.entries(parsedHeaders)) {
+        context.res.headers.set(key, value as string);
+      }
+    } catch (error) {
+      return context.text("Invalid headers format", 400);
+    }
+  }
+
+  if (delay) {
+    const delayMs = parseInt(delay, 10);
+    if (!isNaN(delayMs)) {
+      await new Promise((resolve) => setTimeout(resolve, delayMs));
+    }
+  }
+
+  let overrrideStatusCode: StatusCode | undefined;
+  if (status) {
+    const statusCode = parseInt(status, 10);
+    if (statusCode >= 200 && statusCode <= 599) {
+      overrrideStatusCode = statusCode as StatusCode;
+    } else {
+      return context.text("Invalid status code", 400);
+    }
+  }
+
+  if (overrrideStatusCode && context.res.status !== overrrideStatusCode) {
+    context.status(overrrideStatusCode);
+  }
+  if (body) {
+    return context.text(body);
+  }
+  await next();
+
+});
 
 app.get("/", (context: Context) => {
   const language = context.req.header("Accept-Language") || "en";
@@ -52,7 +100,6 @@ app.get("/", (context: Context) => {
       categories.push({ category, methods: validMethods });
     }
   }
-
   return context.html(<Help categories={categories} />);
 });
 
@@ -91,10 +138,6 @@ app.all("/:category/:method/*", async (context: Context) => {
     return context.text(errorMessage, 400);
   }
 });
-
-function serialize(obj: any) {
-  return JSON.stringify(obj, null, 2);
-}
 
 function createFaker(language: string): any {
   const languageParsed = language.split(",")[0].split("-")[0];
